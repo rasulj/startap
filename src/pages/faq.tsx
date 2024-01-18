6b6331bc65edb041b3b90078e85392bfb3c90215
Пользователь <example@mail.com>
2024-01-18 09:45:54 +0500

    Faq page

---
+++ src/pages/faq.tsx
@@ -0,0 +1,13 @@
+import { withLayout } from "src/layousts/layout"
+import { FaqPageComponent } from "src/page-component"
+
+
+const Faq = () => {
+  return (
+  <>
+  <FaqPageComponent/>
+  </>
+  )
+}
+
+export default withLayout(Faq)
